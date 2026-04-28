571be2b60553cff9ba1329ca04090fdf4e836324
Keola McGowan <keolamcgowan97@outlook.com>
2026-04-28 17:40:27 -0400

    add forcepush move + action effect

---
+++ data/cobblemon/moves/forcepush.js
@@ -0,0 +1,17 @@
+({
+  accuracy: 100,
+  basePower: 65,
+  category: "Special",
+  name: "Force Push",
+  pp: 15,
+  priority: 0,
+  flags: { protect: 1, mirror: 1 },
+  secondary: {
+    chance: 30,
+    boosts: {
+      spe: -1
+    }
+  },
+  target: "normal",
+  type: "Psychic"
+})
